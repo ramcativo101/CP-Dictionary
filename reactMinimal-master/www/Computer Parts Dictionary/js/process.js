ded2940d49d0f7820332b2dc6d4ee232ca5a8aae
reymark cativo <reymark cativo>
2017-11-26 09:43:28 +1000

    CP-DICTIONARY

---
+++ reactMinimal-master/www/Computer Parts Dictionary/js/process.js
@@ -0,0 +1,226 @@
+	var dictionary = [
+
+						{
+							word:"",
+							def:"<h2>Computer Parts Dictionary</h2>",
+							rel:[],
+						},
+
+						{
+							word:"computer case",
+							def:"The computer case is the metal and plastic box that contains the main components of the computer, including the motherboard, central processing unit (CPU), and power supply. The front of the case usually has an On/Off button and one or more optical drives.",
+							rel:["<img src=img/1.jpg width=180>"],
+						},
+
+						{
+							word:"monitor",
+							def:"The monitor works with a video card, located inside the computer case, to display images and text on the screen. Most monitors have control buttons that allow you to change your monitor's display settings, and some monitors also have built-in speakers.",
+							rel:["<img src=img/2.jpg width=160>"],
+						},
+
+						{
+							word:"mouse",
+							def:"The mouse is another important tool for communicating with computers. Commonly known as a pointing device, it lets you point to objects on the screen, click on them, and move them.",
+							rel:["<img src=img/23.jpg width=160>"],
+						},
+
+						{
+							word:"trackball",
+							def:"A trackball has a ball that can rotate freely. Instead of moving the device like a mouse, you can roll the ball with your thumb to move the pointer.",
+							rel:["<img src=img/4.jpg width=160>"],
+						},
+
+						{
+							word:"touchpad",
+							def:"A touchpad—also called a trackpad—is a touch-sensitive pad that lets you control the pointer by making a drawing motion with your finger. Touchpads are common on laptop computers.",
+							rel:["<img src=img/5.jpg width=190>"],
+						},
+
+						{
+							word:"optical disc drive",
+							def:"Often called a CD-ROM or DVD-ROM drive, this lets your computer read CDs and DVDs.",
+							rel:["<img src=img/22.jpg width=160>"],
+						},
+
+						{
+							word:"power button",
+							def:"The power button is used to power the computer on and off.",
+							rel:["<img src=img/27.jpg width=200>"],
+						},
+
+						{
+							word:"audio port",
+							def:"Many computers include audio ports on the front of the computer case that allow you to easily connect speakers, microphones, and headsets without fumbling with the back of the computer.",
+							rel:["<img src=img/19.jpg width=160>"],
+						},
+
+						{
+							word:"usb port",
+							def:"Most desktop computers have several USB ports. These can be used to connect almost any type of device, including mice, keyboards, printers, and digital cameras. They will often appear on the front and back of the computer.",
+							rel:["<img src=img/25.jpg width=160>"],
+						},
+
+						{
+							word:"ethernet port",
+							def:"This port looks a lot like the modem or telephone port, but it is slightly wider. You can use this port for networking and connecting to the Internet.",
+							rel:["<img src=img/26.jpg width=160>"],
+						},
+
+						{
+							word:"monitor port",
+							def:"This is where you'll connect your monitor cable. In this example, the computer has both a DisplayPort and a VGA port. Other computers may have other types of monitor ports, such as DVI (digital visual interface) or HDMI (high-definition multimedia interface).",
+							rel:["<img src=img/21.jpg width=160>"],
+						},
+
+						{
+							word:"serial port",
+							def:"This port is less common on today's computers. It was frequently used to connect peripherals like digital cameras, but it has been replaced by USB and other types of ports.",
+							rel:["<img src=img/17.jpg width=160>"],
+						},
+
+						{
+							word:"ps/2",
+							def:"These ports are sometimes used for connecting the mouse and keyboard. Typically, the mouse port is green and the keyboard port is purple. On new computers, these ports have been replaced by USB.",
+							rel:["<img src=img/16.jpg width=160>"],
+						},
+
+						{
+							word:"expansion slots",
+							def:"These empty slots are where expansion cards are added to computers. For example, if your computer did not come with a video card, you could purchase one and install it here.",
+							rel:["<img src=img/11.jpg width=160>"],
+						},
+
+						{
+							word:"parallel port",
+							def:"This is an older port that is less common on new computers. Like the serial port, it has now been replaced by USB.",
+							rel:["<img src=img/18.jpg width=160>"],
+						},
+
+						{
+							word:"motherboard",
+							def:"The motherboard is the computer's main circuit board. It's a thin plate that holds the CPU, memory, connectors for the hard drive and optical drives, expansion cards to control the video and audio, and connections to your computer's ports (such as USB ports). The motherboard connects directly or indirectly to every part of the computer.",
+							rel:["<img src=img/6.jpg width=160>"],
+						},
+
+						{
+							word:"cpu",
+							def:"The central processing unit (CPU), also called a processor, is located inside the computer case on the motherboard. It is sometimes called the brain of the computer, and its job is to carry out commands. Whenever you press a key, click the mouse, or start an application, you're sending instructions to the CPU.",
+							rel:["<img src=img/7.jpg width=160>"],
+						},
+
+						{
+							word:"ram",
+							def:"RAM is your system's short-term memory. Whenever your computer performs calculations, it temporarily stores the data in the RAM until it is needed.",
+							rel:["<img src=img/8.jpg width=160>"],
+						},
+
+						{
+							word:"hard drive",
+							def:"The hard drive is where your software, documents, and other files are stored. The hard drive is long-term storage, which means the data is still saved even if you turn the computer off or unplug it.",
+							rel:["<img src=img/9.jpg width=160>"],
+						},
+
+						{
+							word:"power supply unit",
+							def:"The power supply unit in a computer converts the power from the wall outlet to the type of power needed by the computer. It sends power through cables to the motherboard and other components.",
+							rel:["<img src=img/10.jpg width=160>"],
+						},
+
+						{
+							word:"video card",
+							def:"The video card is responsible for what you see on the monitor. Most computers have a GPU (graphics processing unit) built into the motherboard instead of having a separate video card. If you like playing graphics-intensive games, you can add a faster video card to one of the expansion slots to get better performance.",
+							rel:["<img src=img/12.jpg width=160>"],
+						},
+
+						{
+							word:"sound card",
+							def:"The sound card—also called an audio card—is responsible for what you hear in the speakers or headphones. Most motherboards have integrated sound, but you can upgrade to a dedicated sound card for higher-quality sound.",
+							rel:["<img src=img/13.jpg width=160>"],
+						},
+
+						{
+							word:"network card",
+							def:"The network card allows your computer to communicate over a network and access the Internet. It can either connect with an Ethernet cable or through a wireless connection (often called Wi-Fi). Many motherboards have built-in network connections, and a network card can also be added to an expansion slot.",
+							rel:["<img src=img/1.jpg width=160>"],
+						},
+
+						{
+							word:"bluetooth card",
+							def:"Bluetooth is a technology for wireless communication over short distances. It's often used in computers to communicate with wireless keyboards, mice, and printers. It's commonly built into the motherboard or included in a wireless network card. For computers that don't have Bluetooth, you can purchase a USB adapter, often called a dongle.",
+							rel:["<img src=img/15.jpg width=120>"],
+						},
+
+						{
+							word:"computer",
+							def:"an electronic device for storing and processing data, typically in binary form, according to instructions given to it in a variable program.",
+							rel:["<img src=img/20.jpg width=160 >"],
+						},
+
+						{/*haha its ramcativo101*/
+							word:"forever",
+							def:"<H1>WALANG FOREVER!!</3</H2>",
+							rel:["<img src=img/.jpg width=160>"],
+						},
+
+						{
+							word:"keyboard",
+							def:"The keyboard is one of the main ways to communicate with a computer. There are many different types of keyboards, but most are very similar and allow you to accomplish the same basic tasks.",
+							rel:["<img src=img/3.jpg width=160>"],
+						},
+
+	];
+
+
+	init = function(){
+		for (var i = 0; i <dictionary.length; i++) {
+			     document.getElementById('word_list').innerHTML += "<li onclick='show(" + i + ")'>" + dictionary[i].word + "</li>";
+		}
+	}
+
+
+	//call the init function when page loads </3
+	init();
+
+	//display a word, its definition and related words</3
+	show = function(i){
+		document.getElementById('word_text').innerHTML = dictionary[i].word;
+		document.getElementById('definition').innerHTML = dictionary[i].def;
+
+		var list = "";
+
+		for (var j = 0; j < dictionary[i].rel.length; j++) {
+			list += "<li>" + dictionary[i].rel[j] + "</li>";
+			document.getElementById('related').innerHTML = list;
+		}
+	}
+
+	//show first word in the dictionary when page loads<3
+	show(0);
+
+	//search functionality not your ex
+	search = function(){
+		query = document.getElementById('search').value;
+
+		if (query == "") {   
+			return;
+		}
+
+		found = -1;
+
+		for (var i = 0; i < dictionary.length; i++) {
+			if (query == dictionary[i].word) {
+				found = i;
+				break;
+			}else {
+				document.getElementById('word_text').innerHTML = "Word not found";
+				document.getElementById('definition').innerHTML = "The word you entered is not found in my dictionary";
+				document.getElementById('related').innerHTML = "No example photo";
+			}
+		}
+
+		if (found >= 0) {
+			show(found);
+		}
+	}
+    /*@cativoram101*/
+
